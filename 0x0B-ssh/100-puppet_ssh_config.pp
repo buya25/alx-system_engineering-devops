5cf1fd77e328327d07974b8db2f979f5f5169ea4
Yabs Mullo <yabsmullo0@gmail.com>
2023-11-24 11:13:19 -0800

    0x0B. SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,14 +2,14 @@
 
 # Ensure the private key is used for authentication
 file_line { 'Use private key for authentication':
-  path    => '~/home/.ssh/2-ssh_config',
+  path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school',
   match   => '^#?IdentityFile',
 }
 
 # Disable password authentication
 file_line { 'Turn off password authentication':
-  path    => '~/home/.ssh/2-ssh_config',
+  path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
   match   => '^#?PasswordAuthentication',
 }
