3a397c5b5e1fdf19b878652ce4256b0382cd4ad2
Yabs Mullo <yabsmullo0@gmail.com>
2023-11-24 10:51:37 -0800

    0x0B. SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,14 @@
 # Configure ssh config file
+include stdlib
 
 file_line { 'Turn off passwd auth':
   path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
-  replace => true
+  line    => 'PasswordAuthentication no',
+  match   => '^#?PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
   path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/school',
-  replace => true
+  line    => 'IdentityFile ~/.ssh/school',
+  match   => '^#?IdentityFile',
 }
