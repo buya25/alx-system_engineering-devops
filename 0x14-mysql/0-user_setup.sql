26656698478a79e4043ba5012ca44a37f3a75e0b
Yabs Mullo <yabsmullo0@gmail.com>
2023-12-18 22:38:27 -0800

    0x14. MySQL

--- 0x14-mysql/0-user_setup.sql
+++ 0x14-mysql/0-user_setup.sql
@@ -1,8 +1,8 @@
 -- script to setup replica user on master sql server
 
-CREATE USER IF NOT EXISTS 'replica_user'@'%' IDENTIFIED BY 'password';
-GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
-GRANT SELECT ON *.* TO 'replica_user'@'%';
+CREATE USER IF NOT EXISTS 'holberton_user'@'%' IDENTIFIED BY 'password';
+GRANT REPLICATION SLAVE ON *.* TO 'holberton_user'@'%';
+GRANT SELECT ON *.* TO 'holberton_user'@'%';
 
 -- check that it was configured properly 
 SELECT user, Repl_slave_priv FROM mysql.user;
