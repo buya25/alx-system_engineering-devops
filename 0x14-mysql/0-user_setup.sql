a8c2d685f48f2c4104b661cab4c8e9ae06c20375
Yabs Mullo <yabsmullo0@gmail.com>
2023-12-19 21:50:28 -0800

    0x14. MySQL

--- 0x14-mysql/0-user_setup.sql
+++ 0x14-mysql/0-user_setup.sql
@@ -1,8 +1,8 @@
 -- script to setup replica user on master sql server
 
-CREATE USER IF NOT EXISTS 'holberton_user'@'%' IDENTIFIED BY 'projectcorrection280hbtn';
+CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
 GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
 FLUSH PRIVILEGES;
-GRANT SELECT ON *.* TO 'holberton_user'@'%';
+GRANT SELECT ON *.* TO 'holberton_user'@'localhost';
 
 mysql -uholberton_user -p -e "SHOW GRANTS FOR 'holberton_user'@'localhost'"
